6d4c146a02bc3fb31190e4c00e62e7ec1f15fe67
damiancupo1982 <damiancupo@gmail.com>
2025-08-18 23:34:48 -0300

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -128,7 +128,7 @@ const Layout: React.FC = () => {
               
               <Home className="h-8 w-8 text-green-600 mr-3" />
               <div>
-                <h1 className="text-xl font-bold text-gray-900">Nuevo Nombre</h1>
+                <h1 className="text-xl font-bold text-gray-900">VNP-San Benito</h1>
                 {isAdmin && (
                   <span className="text-xs text-green-600 font-medium">Modo Administrador</span>
                 )}
